0cd3c895d9de103654106113aaa1d3a55be8a5fe
Skeletoone <Rm83460@estudante.fieb.edu.br>
2023-05-23 10:54:29 -0300

    update package names

--- src/main/java/com/example/miniprojeto3dspring/model/Produto.java
+++ src/main/java/com/example/miniprojeto3dspring/model/Produto.java
@@ -1,4 +1,4 @@
-package br.itb.informatica.miniprojeto3dspring.model;
+package com.example.miniprojeto3dspring.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
--- src/main/java/com/example/miniprojeto3dspring/model/ProdutoRepository.java
+++ src/main/java/com/example/miniprojeto3dspring/model/ProdutoRepository.java
@@ -1,4 +1,4 @@
-package br.itb.informatica.miniprojeto3dspring.model;
+package com.example.miniprojeto3dspring.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
